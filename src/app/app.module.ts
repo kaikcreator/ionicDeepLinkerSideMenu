7a00a36131601edc2b6f03764815f7d5177baceb
kaikcreator <enriqueoriol@gmail.com>
2017-01-09 15:22:58 +0100

    added defaultHistory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { Page3 } from '../pages/page3/page3';
+import { ItemsService } from '../providers/itemsService';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,6 @@ import { Page3 } from '../pages/page3/page3';
     Page2,
     Page3
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, ItemsService]
 })
 export class AppModule {}
